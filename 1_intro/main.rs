d05876f02db579f76c7c56ee0b1d81833822d4ae
Ramazan AYYILDIZ <r@ayyildiz.ai>
2023-07-21 22:56:05 +0300

    first commit

---
+++ 1_intro/main.rs
@@ -0,0 +1,15 @@
+fn main2() {
+    println!("Hello, world!");
+
+    println!("hello world
+sxsa
+xsa
+xsa
+      ");
+
+    println!("your age is {}",10);
+
+    println!("hello {} - {},\n\t how are you?","Ramazan", "AYYILDIZ");
+
+    println!("hello {name}", name="Ramazan");
+}
