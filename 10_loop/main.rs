0a53542a1a34647997a8beb9f7198fa117ec8595
Ramazan A <r@ayyildiz.ai>
2023-07-22 17:22:10 +0300

    loop

---
+++ 10_loop/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+    //let ten_millis = time::Duration::from_millis(100);
+    // loop {
+    //     print!(". ");
+    //     sleep(ten_millis)
+    // }
+
+
+    let mut exit = true;
+    let guess = 32;
+
+    println!("guess a number");
+    while exit {
+        let mut number = String::new();
+
+        std::io::stdin().read_line(&mut number)
+            .expect("failed to get number");
+
+
+        let n :i32 = number.trim().parse().expect("failed to parse");
+        if n == guess {
+            exit = false;
+            println!("congrats, program will exit");
+        }else {
+            println!("wrong, enter again");
+        }
+
+    }
+
+}
