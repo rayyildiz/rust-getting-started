d6855704310a8cc8560bbd14faba9b1ddf5e21fb
Ramazan A <r@ayyildiz.ai>
2023-07-23 16:25:24 +0300

    fn types, closure,

---
+++ examples/21_closure/main.rs
@@ -0,0 +1,54 @@
+fn main() {
+    let x = 5;
+
+    let square = |num: i32| println!("square: {}", num * num);
+    square(x);
+
+    let square = |num: i32| num * num * num;
+    let qube = square(25);
+    println!("qube or square {}", qube);
+
+    let sum = |a, b| a + b;
+    let sum1 = sum(3, 4);
+    println!("sum1 {:?}", sum1);
+
+    // because of first call it is integer, cant be float
+    // let sum2 = sum(2.1, 2.3);
+
+    let can_divide = |a: f32| {
+        if a == 0.0 {
+            false
+        } else {
+            true
+        }
+    };
+
+    let d = divide(4.0, 2.3, can_divide);
+    println!("d :{}", d);
+
+    let mut v = vec![1, 2, 2, 2];
+    let mut add = || {
+        v.push(23);
+    };
+
+    add();
+    println!("vec {:?}", v);
+
+    let v1 = vec![12, 3];
+    let ff = || {
+        let v2 = v1;
+    };
+
+    ff();
+
+    // compile error. because ownership transferred to v2 in the closure
+    //println!("v1 :{:?}",v1);
+}
+
+fn divide<F: Fn(f32) -> bool>(a: f32, b: f32, f: F) -> f32 {
+    if f(b) {
+        a / b
+    } else {
+        0.0
+    }
+}
---
+++ examples/22_functypes/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    let f = max;
+    println!("f :{}", f(2, 3));
+
+    print_info(max, 3, 4);
+    print_info(min, 3, 4);
+
+    let sum = |a: i32, b: i32| a + b;
+    print_info(sum, 3, 4);
+}
+
+fn print_info(f: fn(i32, i32) -> i32, a: i32, b: i32) {
+    println!("hello from info . calling fn");
+    let c = f(a, b);
+    println!("result {}", c);
+}
+
+fn max(a: i32, b: i32) -> i32 {
+    if a > b {
+        a
+    } else {
+        b
+    }
+}
+
+fn min(a: i32, b: i32) -> i32 {
+    if a < b {
+        a
+    } else {
+        b
+    }
+}
