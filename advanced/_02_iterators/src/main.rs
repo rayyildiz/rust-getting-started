f4d2c5943d4808ec05f4f3e5d96175aa96507a67
Ramazan AYYILDIZ <r@ayyildiz.ai>
2023-08-02 20:05:51 +0300

    infinite_loop ( iterator and take implementation)

---
+++ advanced/_02_iterators/src/main.rs
@@ -0,0 +1,51 @@
+use banner::print_banner;
+
+fn main() {
+    print_banner();
+
+    let infinite_loops = Ones;
+
+    for one in infinite_loops.take(5) {
+        print!("{one:?} ");
+    }
+
+    println!("\nno take usage");
+
+    let infinite_loop = Take {
+        remaining_count: 10,
+        inner: Ones,
+    };
+
+    for v in infinite_loop {
+        print!("{v:?} ");
+    }
+}
+
+struct Ones;
+
+impl Iterator for Ones {
+    type Item = i32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        Some(1)
+    }
+}
+
+struct Take<I> {
+    inner: I,
+    remaining_count: usize,
+}
+
+impl<I: Iterator> Iterator for Take<I> {
+    type Item = <I as Iterator>::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.remaining_count {
+            0 => None,
+            _ => {
+                self.remaining_count -= 1;
+                self.inner.next()
+            }
+        }
+    }
+}
